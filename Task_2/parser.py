07aa1a67389c3319463a50e6839b43d83c2bb750
AnupJoseph <anup20joseph@gmail.com>
2020-10-25 12:13:12 +0530

    Added simple parsers

--- Task_2/parser.py
+++ Task_2/parser.py
@@ -1,8 +1,26 @@
+import pandas as pd
+
+
 class ExcelParser(object):
 
+    @staticmethod
+    def simple_drops(dataframe):
+        dataframe = dataframe.drop(dataframe.columns[0], axis=1)
+        dataframe = dataframe.drop(dataframe.index[0], axis=0)
+        return dataframe
+
     def __init__(self, dataframe):
-        self.dataframe = dataframe
+        self.dataframe = simple_drops(dataframe)
+        self.outframe = pd.DataFrame()
+
+    def hydro_parser(self, ):
+        hydro = self.dataframe.loc[self.dataframe['Details'].str.contains(
+            'Hydro')]
+        hydro.loc[:, 'Details'] = 'Hydro'
+        self.dataframe = pd.concat([self.outframe, hydro])
 
     def parse(self, ):
-        dataframe = self.dataframe.copy()
+        self.hydro_parser()
+        print(self.dataframe)
+
         return dataframe
