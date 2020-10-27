991ef56d53604f87c27f667d215d67e635637d30
AnupJoseph <anup20joseph@gmail.com>
2020-10-27 11:28:54 +0530

    Added CLI tool

--- Task_2/main.py
+++ Task_2/main.py
@@ -1,15 +1,26 @@
-from converter import PdfToExcel
+import os
+import sys
+import argparse
+
+from converter import PdfToExcel,DfToCsv
 from parser import ExcelParser
 
+parser = argparse.ArgumentParser()
+
 def transformer(file):
-    pdf = PdfToExcel('150820.pdf')
-    # print(getattr(pdf, 'dataframe'))
+    
+    pdf = PdfToExcel(file)
     exp = ExcelParser(pdf.dataframe)
     extracted_data = exp.parse()
     print(extracted_data)
 
 def main():
-    transformer('150820.pdf')
+    parser.add_argument("-d",'--data',default='./data',action='store_true',dest=data,help="Print the word in upper case letters")
+
+    args = parser.parse_args()
+    if args.data:
+        for files in os.listdir(args.data):
+            transformer(f"{data}/{files}")
 
 if __name__ == "__main__":
     main()
--- Task_2/parser.py
+++ Task_2/parser.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def simple_drops(dataframe):
+    dataframe = dataframe.drop(dataframe.columns[0], axis=1)
+    dataframe = dataframe.drop(dataframe.index[0], axis=0)
+    return dataframe
 
 
 class ExcelParser(object):
-
-    @staticmethod
-    def simple_drops(cls,dataframe):
-        dataframe = dataframe.drop(dataframe.columns[0], axis=1)
-        dataframe = dataframe.drop(dataframe.index[0], axis=0)
-        return dataframe
 
     def __init__(self, dataframe):
         self.dataframe = simple_drops(dataframe)
@@ -22,5 +24,5 @@ class ExcelParser(object):
     def parse(self, ):
         self.hydro_parser()
         print(self.dataframe)
-
+        dataframe = self.outframe.copy()
         return dataframe
