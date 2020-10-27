428897d4afc2b4ea2eff5de8251474eab51b4271
AnupJoseph <anup20joseph@gmail.com>
2020-10-27 12:14:01 +0530

    Finished a full half of all the parsers

--- Task_2/parser.py
+++ Task_2/parser.py
@@ -2,6 +2,8 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
 
 def simple_drops(dataframe):
     dataframe = dataframe.drop(dataframe.columns[0], axis=1)
@@ -18,12 +20,32 @@ class ExcelParser(object):
     def hydro_thermal_gas_parser(self, ):
         combination = self.dataframe.loc[self.dataframe['Details'].str.contains(
             'Hydro') | self.dataframe['Details'].str.contains('Thermal') | self.dataframe['Details'].str.contains('Gas')]
-        combination.at[2,'Details'] = 'Hydro-electricity'
+        combination.at[2, 'Details'] = 'Hydro-electricity'
         self.outframe = pd.concat([self.outframe, hydro])
 
-    def thermal_parser(self, ):
+    def ppn_parser(self,):
+        ppn = self.dataframe.loc[self.dataframe['Details'].str.contains(
+            'PPN') | self.dataframe['Details'].str.contains('ABAN')]
+        self.outframe =  pd.concat([self.outframe, ppn])
+
+    def cpp_parser(self,):
+        cpp = self.dataframe.loc[self.dataframe['Details'].str.contains('CPP') | self.dataframe['Details'].str.contains('Captive')]
+        cpp.index = range(3)
+        cpp.at[2,'Details'] = 'CPP - Wheeling'
+        cpp.reset_index()
+        self.outframe =  pd.concat([self.outframe, cpp])
+    
+    
+    def power_purchase_total_parser(self,):
+        power_total = self.dataframe.loc[self.dataframe['Details'].str.contains(
+            'MTOA') | self.dataframe['Details'].str.contains('Total')]
+        self.outframe =  pd.concat([self.outframe, power_total])
 
     def parse(self, ):
-        self.hydro_parser()
+        self.hydro_thermal_gas_parser()
+        self.ppn_parser()
+        self.cpp_parser()
+
+        self.power_purchase_total_parser()
         dataframe = self.outframe.copy()
         return dataframe
